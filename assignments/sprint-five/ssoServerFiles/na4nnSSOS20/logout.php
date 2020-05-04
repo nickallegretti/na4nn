4bdf597aa9adb22316e7ca88b3df7eee249c1560
nickallegretti <na4nn@mail.missouri.edu>
2020-05-04 00:05:27 -0500

    Sprint 5 Files

---
+++ assignments/sprint-five/ssoServerFiles/na4nnSSOS20/logout.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'config.php';
+ 
+try {
+    if ($adapter->isConnected()) {
+        $adapter->disconnect();
+        //echo 'Logged out the user';
+        //echo '<p><a href="index.php">Login</a></p>';
+        //echo '<br><p><a href="../na4nnProjectsS20.html">Return to Projects Page</a></p>';
+    }
+}
+catch( Exception $e ){
+    echo $e->getMessage() ;
+}
+header("Location: ../na4nnProjectsS20.html"); 
+exit();
